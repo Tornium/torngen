1530aa985f29033e2bd4b9897fa0c1392129b20d
tiksan <webmaster@deek.sh>
2025-07-03 21:23:01 -0500

    Removed unused config environments
    Signed-off-by: tiksan <webmaster@deek.sh>

--- config/config.exs
+++ config/config.exs
@@ -4,7 +4,3 @@ config :torngen,
   file: "openapi.json",
   out_dir: ".out/",
   generator: :elixir
-
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-import_config "#{config_env()}.exs"
